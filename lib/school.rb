6d8835e4c67ad3f4c8470735899058292c28f7f4
Veronica Kim <vero.a.kim@gmail.com>
2018-02-10 20:04:46 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class School
-  attr_reader :roster
+  # attr_reader :roster
 
   def initialize(name)
     @name = name
@@ -10,14 +10,26 @@ class School
     end
   end
 
-  def add_student(name, grade)
+  # def add_student(name, grade)
+  #     if @roster.keys.include?(grade)
+  #         @roster[grade] << name
+  #     else
+  #       @roster[grade] = []
+  #       @roster[grade] << name
+  #     end
+  #     # binding.pry
+  # end
+
+  def add_student(student_name, grade)
+  
       if @roster.keys.include?(grade)
-          @roster[grade] << name
+        @roster[grade] << student_name
+        # binding.pry
       else
         @roster[grade] = []
-        @roster[grade] << name
+        @roster[grade] << student_name
       end
-      # binding.pry
-  end
+    end
+
 
 end
