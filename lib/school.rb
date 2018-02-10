2b627a4e74e7e589bdf3d1899e2d5802176fcf25
Veronica Kim <vero.a.kim@gmail.com>
2018-02-10 20:01:03 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
   end
 
   def add_student(name, grade)
-      if @roster.include?(grade)
+      if @roster.keys.include?(grade)
           @roster[grade] << name
       else
         @roster[grade] = []
