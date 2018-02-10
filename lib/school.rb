d4694b8d523dced9e11361dfd0da7fae3c48eab2
Veronica Kim <vero.a.kim@gmail.com>
2018-02-10 19:04:41 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,10 @@
 class School
   attr_accessor :name, :roster, :length
 
-  def initialize(name)
+  def initialize(name, roster, length)
     @name = name
     @roster = roster
+    @length = length
   end
 
 end
